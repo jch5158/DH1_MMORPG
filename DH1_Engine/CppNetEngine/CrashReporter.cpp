1e77f087763f5e0f0687046b594508da2dd48c81
devhun001 <devhun001@gmail.com>
2026-03-14 13:56:40 +0900

    .

--- DH1_Engine/CppNetEngine/CrashReporter.cpp
+++ DH1_Engine/CppNetEngine/CrashReporter.cpp
@@ -46,7 +46,7 @@ bool CrashReporter::Init(const Wstring& appName, const Wstring& appVersion, cons
     }
 
     const fs::path currentDir = GetExeDirectory();
-    const fs::path handlerPath = currentDir / LR"(..\..\..\Shared\vcpkg_installed\x64-windows-static-md\tools\crashpad\crashpad_handler.exe)";
+    const fs::path handlerPath = currentDir / LR"(crashpad_handler.exe)";
     const fs::path dbPath = currentDir / L"crashes";
     const fs::path metricsPath = currentDir / L"metrics";
 
